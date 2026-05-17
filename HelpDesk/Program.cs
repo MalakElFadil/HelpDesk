7dc93933fb0890c0c91c10da3fbed4402f5eadb5
ElhaddajAya <ayaelhaddaj619@gmail.com>
2026-05-17 01:18:23 +0100

    Phase 3 - tache 1 completee

--- HelpDesk/Program.cs
+++ HelpDesk/Program.cs
@@ -1,6 +1,8 @@
 // Ce fichier est l'entrée point de l'application. Il configure les services et le pipeline de traitement des requêtes HTTP.
 
 using HelpDesk.Data;
+using HelpDesk.Data.Repositories;
+using HelpDesk.Interfaces;
 using HelpDesk.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -12,7 +14,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration
         .GetConnectionString("DefaultConnection")));
 
-// 2. ASP.NET Identity : gestion des comptes et des rôles
+// 2. Enregistrement du repository — injection de dépendances
+builder.Services.AddScoped<ITicketRepository, TicketRepository>();
+
+// 3. ASP.NET Identity : gestion des comptes et des rôles
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
     // Règles du mot de passe simplifiées pour le développement
@@ -24,10 +29,10 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 .AddEntityFrameworkStores<ApplicationDbContext>()
 .AddDefaultTokenProviders();
 
-// 3. MVC
+// 4. MVC
 builder.Services.AddControllersWithViews();
 
-// 4. Rediriger vers /Account/Login si non connecté
+// 5. Rediriger vers /Account/Login si non connecté
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Account/Login";
